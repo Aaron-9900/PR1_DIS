a754dbba75b9b9117a9f422422e473d5a9f898dc
Aaron <Aaronhoffman1999@gmail.com>
2020-03-13 15:46:01 +0100

    Add product option (should read from xml in the future

--- xml_json_feed/src/xml_feed/Main.java
+++ xml_json_feed/src/xml_feed/Main.java
@@ -3,6 +3,16 @@
 package xml_feed;
 
 import java.io.BufferedReader;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -27,10 +37,10 @@ public class Main {
 	public static void generateMenu() 
 	{
 		System.out.println("**************************");
-		System.out.println("1.- Enter data");
-		System.out.println("2.- Save to xml");
-		System.out.println("3.- Export to xml");
-		System.out.println("4.- validate to Xml");
+		System.out.println("1.- Introducir productos en fichero");
+		System.out.println("2.- Leer xml");
+		System.out.println("3.- validate to Xml");
+		System.out.println("4.- Mocks");
 		System.out.println("0.- Quit");
 	}
 	
@@ -44,19 +54,59 @@ public class Main {
 		System.out.println("0.- Quit");
 	}
 	
-	public static Alumno showSubmenu() throws IOException 
+	public static ArrayList<Producto> introducirProductos() throws IOException 
 	{
 		java.io.BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String name = null;
-		String surname = null;
-		System.out.println("Enter name:");
-		name = in.readLine();
-		System.out.println("Enter surname:");
-		surname = in.readLine();
-		Alumno alumn = new Alumno(name, surname);
-		return alumn;
+		ArrayList<Producto> listaProductos = new ArrayList<Producto>();
+		int input = 0;
+		int iter = 0;
+		do {
+			if(iter == 0) {
+				System.out.println("Ahora está introducciendo un producto.");
+			}
+			System.out.println("Nombre:");
+			String nombre = in.readLine();
+			System.out.println("Codigo:");
+			Integer codigo = Integer.parseInt(in.readLine());
+			System.out.println("Descripcion:");
+			String descripcion = in.readLine();
+			System.out.println("Stock:");
+			Integer stock = Integer.parseInt(in.readLine());			
+			System.out.println("Pasillo:");
+			Integer pasillo = Integer.parseInt(in.readLine());
+			System.out.println("Estanteria:");
+			Integer estanteria = Integer.parseInt(in.readLine());
+			System.out.println("Estante:");
+			Integer estante = Integer.parseInt(in.readLine());
+			System.out.println("Pendientes:");
+			Integer pendientes = Integer.parseInt(in.readLine());
+			
+			Localizacion localizacion = new Localizacion(pasillo, estanteria, estante);
+			Producto producto = new Producto(codigo, nombre, descripcion, stock, localizacion, pendientes);
+			
+			listaProductos.add(producto);
+			
+			System.out.println("¿Desea introducir otro producto?");
+			System.out.println("1- Si");
+			System.out.println("2- No");
+			input = Integer.parseInt(in.readLine());
+			
+			
+		}while(input != 2);
+		String productosXML = getProductListXML(listaProductos);
+		productosXML = formatXML(productosXML);
+		writeToFile(productosXML, "productos.xml");
+		return listaProductos;
 	}
-	
+	private static String getProductListXML(ArrayList <Producto>productos) {
+		String sol = "";
+		sol += "<productos>";
+		for(int i = 0; i < productos.size(); i++) {
+			sol += productos.get(i).toString() + "";
+		}
+		sol += "</productos>";
+		return sol;
+	}
 	public static String formatXML(String input) {
         Source xmlInput = new StreamSource(new StringReader(input));
         StringWriter stringWriter = new StringWriter();
@@ -95,7 +145,19 @@ public class Main {
 		}
 	}
 	
-
+	public static void readXML(String name) throws SAXException, IOException {
+		File fXmlFile = new File(name);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = null;
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		Document doc = dBuilder.parse(fXmlFile);
+		System.out.println(doc.getDocumentElement().getTextContent());
+		
+	}
 	public static void main(String[] args) throws IOException, Exception {
 			
 			ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
@@ -110,25 +172,28 @@ public class Main {
 				switch (menu) 
 				{
 					case 1:
-						Alumno alumno_leido = showSubmenu();
-						alumnos.add(alumno_leido);
+						// Pedido pedido = showSubmenu();
+						introducirProductos();
 						break;
 					case 2:
-						String header = "<?xml version=\"1.0\" encoding=\"UTF-8\">\n<!DOCTYPE pedido SYSTEM  \"pedidos.dtd\"\r\n" + 
-								"";
-						String root = "<clase>\n";
-						String xml = "";
-						xml += header + root;
-						for (Alumno a: alumnos) 
-						{
-							xml += a.toXml();
-						}
-						String close_root = "</clase>";
-			
-						xml += close_root;
-	
-						System.out.println(xml);
-						writeToFile(xml, "xmlFile.xml");
+						/*
+							String header = "<?xml version=\"1.0\" encoding=\"UTF-8\">\n<!DOCTYPE pedido SYSTEM  \"pedidos.dtd\"\r\n" + 
+									"";
+							String root = "<clase>\n";
+							String xml = "";
+							xml += header + root;
+							for (Alumno a: alumnos) 
+							{
+								xml += a.toXml();
+							}
+							String close_root = "</clase>";
+				
+							xml += close_root;
+		
+							System.out.println(xml);
+							writeToFile(xml, "xmlFile.xml");
+						*/
+						readXML("productos.xml");
 						break;
 					case 3:
 						System.out.println(XMLUtils.validateWithDTDUsingDOM("xmlFile.xml"));
@@ -156,7 +221,7 @@ public class Main {
 						xm = formatXML(xm);
 						System.out.println(xm);
 						
-						writeToFile(xm, "JsonFile.xml");
+						writeToFile(xm, "xmlFile.xml");
 						break;
 				}
 				System.out.println("Saliendo del programa");
